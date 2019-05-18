059ecc0c7639a6e11a3ac44ea30afd3990b92108
Sateesh Kavuri <sateesh.kavuri@gmail.com>
2019-05-18 23:31:24 +0530

    Added method to get the facilities of type room items
    Signed-off-by: Sateesh Kavuri <sateesh.kavuri@gmail.com>

--- src/db/Hotel.js
+++ src/db/Hotel.js
@@ -4,7 +4,8 @@
  */
 'use strict';
 
-var _ = require('lodash');
+var _ = require('lodash'),
+    Fuse = require('fuse.js');
 let Conn = require('./Conn');
 const TableName = 'Hotel';
 
@@ -48,14 +49,47 @@ module.exports.facility_names = async function(hotel_id) {
         throw error;
     }
 
-
-
     return data;
+}
+
+module.exports.room_items = async function(hotel_id, f_type) {
+    let params = {
+        TableName: TableName,
+        Key: {
+            'hotel_id': hotel_id
+        },
+        AttributesToGet: ['facilities']
+    };
+
+    let data;
+    try {
+        data = await Conn().get(params).promise();
+    } catch (error) {
+        console.error('error while getting hotel facilities:', hotel_id, error);
+        throw error;
+    }
+
+    let fuse_options = {
+        shouldSort: true,
+        threshold: 0.5,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 3,
+        keys: [
+          "f_type"
+        ]
+    };
+
+    var fuse = new Fuse(data.Item.facilities, fuse_options);
+    var result = fuse.search(f_type);
+
+    return result;
 }
 
 const test = async function() {
     let Hotel = require('./Hotel');
-    var p = await Hotel.facility_names("100");
+    var p = await Hotel.room_items("100", "r");
     console.log('data=', JSON.stringify(p));
 }
 
