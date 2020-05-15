db06c95e4f43c56c2a18d3a926d4a3e5c901614e
kavuri <sateesh.kavuri@gmail.com>
2020-05-15 18:51:30 +0530

    Fixed bug for orderListener
    Added AUTH0 configuration to .env.dev
    
    Signed-off-by: kavuri <sateesh.kavuri@gmail.com>

--- api/lib/ordersListener.js
+++ api/lib/ordersListener.js
@@ -34,7 +34,7 @@ module.exports.sendEvent = function (hotel_id, data) {
     // console.log('sending data to ', hotel_id, data)
 
     Object.keys(clients[hotel_id]).forEach(c => {
-        console.log('clientId=', c)
+        //console.log('clientId=', c)
         clients[hotel_id][c].write(`data: ${JSON.stringify(data)}\n\n`)
     });
 }
@@ -42,21 +42,21 @@ module.exports.sendEvent = function (hotel_id, data) {
 module.exports.watchOrders = () => {
     console.log('watching for changes in orders...');
     OrderModel.watch({ fullDocument: 'updateLookup' }).on('change', async (data) => {
-        console.log('order changed:', JSON.stringify(data), '+++\n', data.fullDocument);
+        //console.log('order changed:', JSON.stringify(data), '+++\n', data.fullDocument);
 
         if (_.isEqual(data.operationType, 'delete')) { // Document is deleted. Should not be!
             //TODO: Documents should never be deleted. Maybe add an audit log?
         } else if (_.isEqual(data.operationType, 'insert') || _.isEqual(data.operationType, 'update')) {
             // This is triggered when the guest changes state (like cancel order) or increases the priority (like I need the item urgently)
             // FIXME: If the update is done from front desk, the event will make a round trip - inefficient
-            const hotel_id = data.hotel_id;
+            const hotel_id = data.fullDocument.hotel_id;
             let checkincheckout = await CheckinCheckout
                 .findById(data.fullDocument.checkincheckout, '-orders')
                 .lean()
                 .exec();
             let order = data.fullDocument;
             order['checkincheckout'] = [checkincheckout];   // This is to maintain consistency with the GET of orders, where checkincheckout is returned as an array
-            console.log('+++document=', order);
+            console.log('sending ' + JSON.stringify(order.item) + ' to ' + hotel_id);
             this.sendEvent(hotel_id, order);
         }
     });
