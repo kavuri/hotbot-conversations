a9d31a4896790c9f8ad6b3e9839155afc6d15bb2
Kavuri <sateesh.kavuri@gmail.com>
2020-12-11 17:17:38 +0530

    Update cmds.sh

--- config/aws/cmds.sh
+++ config/aws/cmds.sh
@@ -1,6 +1,6 @@
-pool_id='ap-south-1_M1pQu9BIA'
-client_id='1v9nbc9ck9uo242tt9429ptcn4'
-domain='kamamishu1'
+pool_id='*****'
+client_id='****'
+domain='****'
 aws cognito-idp describe-user-pool --user-pool-id $pool_id > cognito_userpool.cfg
 aws cognito-idp describe-user-pool-client --user-pool-id $pool_id --client-id $client_id > user_pool_Alexa_client.json
 aws cognito-idp describe-user-pool-client --user-pool-id $pool_id --client-id $client_id > user_pool_api_client.json
